750cf58c2e79664feaf451f4f8dcfa5cc138f87b
Murillo ROmeu <murillosi@gmail.com>
2021-06-15 08:22:40 -0300

    implementando mailtrap

--- src/providers/implementations/MailTrapMailProvider.ts
+++ src/providers/implementations/MailTrapMailProvider.ts
@@ -1,7 +1,33 @@
 import { IMailProvider, IMessage } from "../IMailProvider";
+import nodemailer from 'nodemailer'
+import Mail from "nodemailer/lib/mailer";
 
 export class MailTrapMailProvider implements IMailProvider{
+  private transporter: Mail;
+
+  constructor(){
+    this.transporter = nodemailer.createTransport({
+      host: "smtp.mailtrap.io",
+      port: 2525,
+      auth: {
+        user: "61d2531f9883b7",
+        pass: "63a8ad80d4fa60"
+      }
+    })
+  }
+  
   async sendMail(message: IMessage): Promise<void> {
-    throw new Error("Method not implemented.");
+    await this.transporter.sendMail({
+      to: {
+        name: message.to.name,
+        address: message.to.email
+      },
+      from: {
+        name: message.from.name,
+        address: message.from.email
+      },
+      subject: message.subject,
+      html: message.body
+    })
   }
 }
